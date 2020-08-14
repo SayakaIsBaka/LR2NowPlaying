ba290792a2b2ad9780eb35210beb46022ed940fc
Jérôme Tchan <jerome.tchan@epita.fr>
2020-08-14 19:51:24 +0200

    refactor code + added basic udp client

---
+++ LR2NowPlaying/MindReceiver.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace LR2NowPlaying
+{
+    class MindReceiver
+    {
+        private int port;
+        private UdpClient udpClient;
+
+        public MindReceiver(int port)
+        {
+            this.port = port;
+            udpClient = new UdpClient();
+            udpClient.Client.Bind(new IPEndPoint(IPAddress.Any, port));
+        }
+
+        public void Listen()
+        {
+            IPEndPoint from = new IPEndPoint(0, 0);
+            while (true)
+            {
+                byte[] recvBuffer = udpClient.Receive(ref from);
+                string str = Encoding.UTF8.GetString(recvBuffer).Substring(1);
+                Console.WriteLine(str);
+                using (var md5 = MD5.Create())
+                {
+                    using (var stream = File.OpenRead(str))
+                    {
+                        var hash = md5.ComputeHash(stream);
+                        Console.WriteLine(BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant());
+                    }
+                }
+            }
+        }
+    }
+}
--- LR2NowPlaying/Program.cs
+++ LR2NowPlaying/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Runtime.InteropServices;
-using System.Text;
+using System.Threading;
 
 namespace LR2NowPlaying
 {
@@ -9,55 +7,20 @@ namespace LR2NowPlaying
     {
         static int Main(string[] args)
         {
-            Process[] lr2Processes = Process.GetProcessesByName("LRHbody");
-            if (lr2Processes.Length == 0)
-            {
-                Console.Error.WriteLine("LRHbody process not found");
-                return 1;
-            }
-            Process lr2Process = lr2Processes[0];
-            String dllPath = AppDomain.CurrentDomain.BaseDirectory + "/LR2mind.dll";
-            uint dllPathLength = (uint)((dllPath.Length + 1) * Marshal.SizeOf(typeof(char)));
+            string dllPath = AppDomain.CurrentDomain.BaseDirectory + "/LR2mind.dll";
+            string processName = "LRHbody";
 
-            IntPtr lr2ProcHandle = Imports.OpenProcess(Imports.PROCESS_CREATE_THREAD | Imports.PROCESS_QUERY_INFORMATION | Imports.PROCESS_VM_OPERATION | Imports.PROCESS_VM_WRITE | Imports.PROCESS_VM_READ, false, lr2Process.Id);
-            if (lr2ProcHandle == IntPtr.Zero)
-            {
-                Console.Error.WriteLine("error while opening LRHbody process");
-                return 1;
-            }
+            Injector injector = new Injector(processName);
+            int ret = injector.Inject(dllPath);
 
-            IntPtr loadLibraryAddr = Imports.GetProcAddress(Imports.GetModuleHandle("kernel32.dll"), "LoadLibraryA");
-            IntPtr mindBaseAddress = Imports.VirtualAllocEx(lr2ProcHandle, IntPtr.Zero, dllPathLength, Imports.MEM_COMMIT, Imports.PAGE_EXECUTE_READWRITE);
-
-            UIntPtr bytesWritten;
-            Imports.WriteProcessMemory(lr2ProcHandle, mindBaseAddress, Encoding.Default.GetBytes(dllPath), dllPathLength, out bytesWritten);
-
-            IntPtr loadThread = Imports.CreateRemoteThread(lr2ProcHandle, IntPtr.Zero, 0, loadLibraryAddr, mindBaseAddress, 0, IntPtr.Zero);
-            if (loadThread == IntPtr.Zero)
-            {
-                Console.Error.WriteLine("error while creating thread");
-                return 1;
-            }
-
-            return 0;
-
-            // Cleaning process
-
-            Imports.WaitForSingleObject(loadThread, Imports.INFINITE);
-            IntPtr baseAddressMind;
-            Imports.GetExitCodeThread(loadThread, out baseAddressMind);
-            Console.WriteLine("load_library_thread - detached " + baseAddressMind);
-
-            Imports.CloseHandle(loadThread);
-            Imports.VirtualFreeEx(lr2ProcHandle, mindBaseAddress, 0, Imports.MEM_RELEASE);
-
-            IntPtr freeLibraryAddr = Imports.GetProcAddress(Imports.GetModuleHandle("kernel32.dll"), "FreeLibrary");
-            IntPtr freeThread = Imports.CreateRemoteThread(lr2ProcHandle, IntPtr.Zero, 0, freeLibraryAddr, baseAddressMind, 0, IntPtr.Zero);
-
-            Imports.WaitForSingleObject(freeThread, Imports.INFINITE);
-            Imports.CloseHandle(freeThread);
-
-            return 0;
+            Thread receiverThread = new Thread(
+                delegate ()
+                {
+                    MindReceiver receiver = new MindReceiver(2222);
+                    receiver.Listen();
+                });
+            receiverThread.Start();
+            return ret;
         }
     }
 }
