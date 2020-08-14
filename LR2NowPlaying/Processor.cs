f8657fd76e4866117895f833338d19d38b332ebf
Jérôme Tchan <jerome.tchan@epita.fr>
2020-08-14 22:32:53 +0200

    added request to LR2IR, no parsing of response yet

--- LR2NowPlaying/Processor.cs
+++ LR2NowPlaying/Processor.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.IO;
+using System.Net.Http;
 using System.Security.Cryptography;
+using System.Threading.Tasks;
 
 namespace LR2NowPlaying
 {
     class Processor
     {
         private BlockingCollection<string> queue;
-        
+        static readonly HttpClient client = new HttpClient();
+
         public Processor(BlockingCollection<string> queue)
         {
             this.queue = queue;
@@ -16,14 +19,27 @@ namespace LR2NowPlaying
 
         public void Process()
         {
-            string str = queue.Take();
+            string bmsPath = queue.Take();
             using (MD5 md5 = MD5.Create())
             {
-                using (FileStream stream = File.OpenRead(str))
+                using (FileStream stream = File.OpenRead(bmsPath))
                 {
                     byte[] hash = md5.ComputeHash(stream);
                     string hashStr = BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
                     Console.WriteLine(hashStr);
+
+                    Task.Run(async () =>
+                    {
+                        try
+                        {
+                            string responseBody = await client.GetStringAsync("http://www.dream-pro.info/~lavalse/LR2IR/search.cgi?mode=ranking&bmsmd5=" + hashStr);
+                            Console.WriteLine(responseBody);
+                        }
+                        catch (HttpRequestException e)
+                        {
+                            Console.Error.WriteLine(e);
+                        }
+                    });
                 }
             }
         }
